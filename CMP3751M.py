ecce3f6f90f182b24e00b792a037ff1879033dcc
Peter Maltby <Peter.p.maltby@gmail.com>
2021-02-02 20:16:26 +0000

    neurel network complete with learning grpah created.

--- CMP3751M.py
+++ CMP3751M.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot
 import sklearn.preprocessing
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 from sys import getsizeof
 print("numpy version: ", numpy.__version__)
 print("pands version: ", pandas.__version__)
@@ -30,6 +31,8 @@ data_cancerous = data.loc[data['Status'] == "cancerous"]
 matplotlib.pyplot.boxplot([data_healthy["Age"],data_cancerous["Age"]])
 matplotlib.pyplot.title("Boxplot of clinical_dataset.xlsx age, categorised by status.")
 matplotlib.pyplot.xticks([1, 2], ['Healthy', "Cancerous"])
+matplotlib.pyplot.xlabel("Status")
+matplotlib.pyplot.ylabel("Age (years)")
 matplotlib.pyplot.savefig('s1Boxplot')
 
 #histogram of BMI classfied by status.
@@ -37,6 +40,8 @@ matplotlib.pyplot.clf()
 matplotlib.pyplot.hist([data_healthy["BMI"],data_cancerous["BMI"]])
 matplotlib.pyplot.title("Density plot of clinical_dataset.xlsx BMI, categorised by status.")
 matplotlib.pyplot.legend(['Healthy', "Cancerous"])
+matplotlib.pyplot.xlabel("BMI")
+matplotlib.pyplot.ylabel("Density")
 matplotlib.pyplot.savefig('s1Densityplot')
 
 #catagorical data converted to bool where 0=healthy and placed in output array (y).
@@ -58,7 +63,31 @@ print(y)
 #split data between test and train set 1:9 ratio.
 x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.1)
 
-net = MLPClassifier((500,500),'logistic',max_iter=500);
-net.fit(x_train, y_train)
+
+accuracyScores_test = []
+accuracyScores_train = []
+
+iter = 500
+net = MLPClassifier((500,500),'logistic',max_iter=iter);
+
+for n in range (1,iter):
+    
+    net.partial_fit(x_train, y_train, classes=numpy.unique(y_train))
+
+    predicted = net.predict(x_test)
+    accuracyScores_test.append(accuracy_score(y_test, predicted))
+    print ("accuracy of neural network = ", accuracy_score(y_test, predicted))
+    predicted = net.predict(x_train)
+    accuracyScores_train.append(accuracy_score(y_train, predicted))
+    
+matplotlib.pyplot.clf()
+matplotlib.pyplot.plot(range(1,iter),accuracyScores_test, 'r--',label = 'test dataset accuracy')
+matplotlib.pyplot.plot(range(1,iter),accuracyScores_train,'b--',label = 'train dataset accuracy')
+matplotlib.pyplot.legend()
+matplotlib.pyplot.title("Accuracy of neural network prediction over data iterations.")
+matplotlib.pyplot.xlabel("epochs")
+matplotlib.pyplot.ylabel("accuracy (%)")
+matplotlib.pyplot.savefig('S3AccuracyPlot')
 
 print("output function: ", net.out_activation_)
+
